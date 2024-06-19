6a804bde6f025aea9af9723aa414a9bac477cf56
Henok <henitadesse97@gmail.com>
2024-06-19 10:33:08 -0300

    Update main.py

--- main.py
+++ main.py
@@ -74,7 +74,7 @@ async def button_handler(update:Update, context:ContextTypes.DEFAULT_TYPE):
 
 
 if __name__ == '__main__':
-    app = ApplicationBuilder().token("token").build()
+    app = ApplicationBuilder().token(token).build()
     app.add_handler(CommandHandler("start", start))
     app.add_handler(CallbackQueryHandler(button_handler))
     app.run_polling()
